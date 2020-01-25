7e819fb4e25fff0a76f704df6ae52ce4eb9d6b6c
Jen Valladares <lilspikey333@yahoo.com>
2020-01-25 10:14:18 -0600

    review done to a point - we need to make a call on the routes

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -3,8 +3,18 @@ const router = express.Router();
 const review = require("../models/Review");
 
 router.get("/", (req, res) => {
-    review.find({}).then(reviews => res.json(reviews))
-})
+  review.find({}).then(reviews => res.json(reviews));
+});
+
+router.get("/:id", (req, res) => {
+  review.findOne({ _id: req.params.id }).then(review => res.json(review));
+});
+
+router.post("/", (req, res) => {
+  review.create(req.body).then(review => {
+    res.json(review);
+  });
+});
 
 
 module.exports = router;
