3a4169175a69bc07b5aa70f75aa02710a84adbe0
ngumina17 <nicole.gumina17@gmail.com>
2020-01-29 09:11:34 -0600

    new put route for adding a review

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Restaurant = require("../models/Restaurant");
+const Review = require('../models/Review')
 
 router.get("/", (req, res) => {
   Restaurant.find({}).then(restaurants => res.json(restaurants));
@@ -38,6 +39,27 @@ router.put("/:id", (req, res) => {
     res.json(restaurant);
   });
 });
+
+router.put("/:id/:reviewId", (req, res) => {
+    let restaurantID = req.params.id;
+    let reviewID = req.params.reviewId;
+  
+    // find the bookmark by its id
+    Restaurant.findById(reviewID).then(singleRestaurant => {
+      // find the user by its id
+      // could also swap this out with email
+      Review.findOneAndUpdate({ _id: restaurantID }).then(user => {
+        // push each id into the others array
+        reviews.favorites.push(singleRestaurant._id);
+        singleRestaurant.favorited.push(reviews._id);
+        // save both
+        reviews.save();
+        singleRestaurant.save();
+        // send json response
+        res.json(reviews);
+      });
+    });
+  });
 
 router.delete("/:id", (req, res) => {
   let id = req.params.id;
